a7e8a2966aa983339c9572616a2fbfe3e15494ab
Eli Summers <40410999+NaNe3@users.noreply.github.com>
2026-05-03 16:59:16 -0600

    made the code more readable

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        Resume myResume = new Resume();
+        myResume._fullName = "Allison Rose";
+
         Job firstJob = new Job();
         firstJob._jobName = "Software Engineer";
         firstJob._companyName = "Microsoft";
@@ -16,8 +20,6 @@ class Program
         secondJob._startYear = 2022;
         secondJob._endYear = 2023;
 
-        Resume myResume = new Resume();
-        myResume._fullName = "Allison Rose";
         myResume._jobs.Add(firstJob);
         myResume._jobs.Add(secondJob);
 
