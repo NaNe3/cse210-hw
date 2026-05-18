046f9afb9c66445357f5b2a45bfabc47c8ad1173
Eli Summers <40410999+NaNe3@users.noreply.github.com>
2026-05-17 21:11:11 -0600

    finished weekly learning activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,36 @@
 using System;
 
+// THE SUPER AWESOME PROGRAM TO CREATE FRACTIONS AND STUFF!!!
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(3, 4);
+        Fraction f4 = new Fraction(1, 3);
+
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        f1.SetTop(7);
+        f1.SetBottom(8);
+        Console.WriteLine($"After setter: {f1.GetTop()}/{f1.GetBottom()}");
+
+        Fraction randomFractionForTheDisplays = new Fraction();
+        Random rng = new Random();
+
+        for (int i = 1; i <= 20; i++)
+        {
+            randomFractionForTheDisplays.SetTop(rng.Next(1, 100));
+            randomFractionForTheDisplays.SetBottom(rng.Next(1, 100));
+            Console.WriteLine($"Fraction {i}: string: {randomFractionForTheDisplays.GetFractionString()} Number: {randomFractionForTheDisplays.GetDecimalValue()}");
+        }
     }
 }
