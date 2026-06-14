070ef7d00289cc65bb695a78a217440bf49d7cee
Eli Summers <40410999+NaNe3@users.noreply.github.com>
2026-06-14 16:19:07 -0600

    finishing prove portion of polymorphism activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Red", 4);
+        Console.WriteLine($"Square color: {square.GetColor()}, area: {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Blue", 5, 3);
+        Console.WriteLine($"Rectangle color: {rectangle.GetColor()}, area: {rectangle.GetArea()}");
+
+        Circle circle = new Circle("Green", 2.5);
+        Console.WriteLine($"Circle color: {circle.GetColor()}, area: {circle.GetArea()}");
+
+        List<Shape> shapes = new List<Shape>
+        {
+            square,
+            rectangle,
+            circle,
+            new Square("Yellow", 2),
+            new Rectangle("Purple", 6, 2),
+            new Circle("Orange", 1.5)
+        };
+
+        Console.WriteLine("\nShapes in list:");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
